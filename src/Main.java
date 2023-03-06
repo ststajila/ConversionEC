6da7c18a24eeece5a8af459f2bc572fe69a67ead
Stanislav Stajila <107271739+ststajila@users.noreply.github.com>
2023-03-06 17:41:27 +0000

    Support convertion up to base 16

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int num = scan.nextInt();
         System.out.println("In what base?");
         int b = scan.nextInt();
-        System.out.println( num + " in base " + b + " is" + convert(num, b));
+        System.out.println( num + " in base " + b + " is " + convert(num, b));
 
     }
 
@@ -17,18 +17,45 @@ public class Main {
         int remainder; // the remainder when num is divided by base b
         quotient = num/b;
         remainder = num%b;
-        if ( quotient == 0 ) //fill in base case
+        if (quotient == 0) //fill in base case
         {
-            return ("" + remainder );
+            return ("" + support(remainder));
         }
         else
         {
 // Recursive step: the number is the base b representation of
 // the quotient concatenated with the remainder
 
-            return ( convert(quotient, remainder) + "" + remainder);
+            return ( convert(quotient, b) + "" + support (remainder));
+       
         }
     }
 
+  public static String support(int num){
+    if (num < 10){
+      return ""+ num;
+    }
+    else if (num == 10){
+      return "A";
+    }
+    else if (num == 11){
+      return "B";
+    }
+    else if (num == 12){
+      return "C";
+    }
+    else if (num == 13){
+      return "D";
+    }
+    else if (num == 14){
+      return "E";
+    }
+    else if (num == 15){
+      return "F";
+    }
+    else{
+      return "G";
+    }
 
+}
 }
